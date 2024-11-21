833395b8495bc5df142008fce408a4e753ef6742
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-11-21 13:58:31 +0100

    feat: add models and migrations

---
+++ app/models/client.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Opportunity from './opportunity.js'
+
+export default class Client extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @column()
+  declare lastName: string
+
+  @column()
+  declare firstName: string
+
+  @column()
+  declare email: string
+
+  @hasMany(() => Opportunity)
+  declare opportunities: HasMany<typeof Opportunity>
+}
---
+++ app/models/opportunity.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Client from './client.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export type OpportunityStatusType = 'progress' | 'validated' | 'cancelled'
+
+export default class Opportunity extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @column()
+  declare successOpportunity: number
+
+  @column()
+  declare status: OpportunityStatusType
+
+  @column()
+  declare clientId: number
+
+  @belongsTo(() => Client)
+  declare client: BelongsTo<typeof Client>
+}
