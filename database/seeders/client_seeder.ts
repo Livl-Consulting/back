bf3570b0e2993d790e9ca09a40fdae548c6cc3e6
Julien Von Der Marck <vdmjulien05@gmail.com>
2025-02-02 21:37:54 +0100

    fix: remove id on seed

--- database/seeders/client_seeder.ts
+++ database/seeders/client_seeder.ts
@@ -5,28 +5,24 @@ export default class ClientSeeder extends BaseSeeder {
   async run() {
     await Client.createMany([
       {
-        id: 1,
         firstName: 'Olivier',
         lastName: 'Bonhomme',
         companyName: 'Airbus',
         email: 'olivier.bonhomme@airbus.fr',
       },
       {
-        id: 2,
         firstName: 'Neil',
         lastName: 'Armstrong',
         companyName: 'NASA',
         email: 'neil.armstrong@nasa.com',
       },
       {
-        id: 3,
         firstName: 'Jean Marc',
         lastName: 'MULLER',
         companyName: 'UNISTRA',
         email: 'jean.marce@muller.fr',
       }, 
       {
-        id: 4,
         firstName: 'Jeane',
         lastName: 'Barsegian',
         companyName: 'Eurométropole',
--- database/seeders/product_seeder.ts
+++ database/seeders/product_seeder.ts
@@ -5,76 +5,65 @@ export default class ProductSeeder extends BaseSeeder {
   async run() {
     await Product.createMany([
       {
-        id: 1,
         name: 'Gestion de projet',
         price: 15000,
         description: 'Gestion de projet en agilité',
         type: 'sale'
       },
       {
-        id: 2,
         name: 'Developpement',
         price: 20000,
         description: 'Tous types de développement',
         type: 'sale'
       },
       {
-        id: 3,
         name: 'Hébergement',
         price: 100,
         description: 'Hébergement des sites web',
         type: 'purchase'
       },
       {
-        id: 4,
         name: 'Prestataire testeur Tchétchénien',
         price: 400,
         description: 'Achat et location de prestataire Tchétchénien',
         type: 'both'
       },
       {
-        id: 5,
         name: 'Application de Complex Event Processing',
         price: 150000,
         description: 'Application native iOS',
         type: 'sale'
       }, 
       {
-        id: 6,
         name: 'Installation de base de données',
         price: 5000,
         description: 'Installation de base de données',
         type: 'sale'
       },
       {
-        id: 7,
         name: 'Réalisation de vols assistés par IA',
         price: 100000,
         description: 'Réalisation de vols assistés par IA',
         type: 'sale'
       },
       {
-        id: 8,
         name: 'Lecteurs codes-barres',
         price: 100,
         description: 'Lecteur de code barre pour la caisse',
         type: 'purchase'
       },
       {
-        id: 9,
         name: 'Tiroirs-caisse',
         price: 1000,
         description: 'Tiroirs-caisse pour la caisse',
         type: 'purchase'
       }, 
       {
-        id: 10,
         name: 'Imprimantes tickets',
         price: 500,
         description: 'Imprimantes tickets pour la caisse',
         type: 'purchase'
       }
-
     ])
   }
 }
