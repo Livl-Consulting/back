5ceae7b33e856b1ee94ef0eebe9c30cdbc9f0f10
Raffael Di Pietro <maxou4170@gmail.com>
2025-02-02 14:41:54 +0100

    lint

--- app/controllers/meuch_controller.ts
+++ app/controllers/meuch_controller.ts
@@ -2,19 +2,18 @@ import env from '#start/env'
 import type { MeuchEntry } from '../types/meuch_map.js'
 
 export default class MeuchController {
-
-    public async meuch(): Promise<MeuchEntry[]> {
-        return [
-        {
-            key: `${env.get("API_CODE")}_LIST_CLIENT`,
-            endpoint: `/api/clients`,
-            description: 'Récupère tout les clients',
-            type: 'GET',
-            routeFormat: null,
-            queryParams: null,
-            body: null,
-            response: null
-        }]
-    }
-
+  public async meuch(): Promise<MeuchEntry[]> {
+    return [
+      {
+        key: `${env.get('API_CODE')}_LIST_CLIENT`,
+        endpoint: `/api/clients`,
+        description: 'Récupère tout les clients',
+        type: 'GET',
+        routeFormat: null,
+        queryParams: null,
+        body: null,
+        response: null,
+      },
+    ]
+  }
 }
--- app/services/meuch_service.ts
+++ app/services/meuch_service.ts
@@ -1,14 +1,14 @@
 import env from '#start/env'
 
-import axios from "axios";
+import axios from 'axios'
 
 export class MeuchService {
   public async registerToMiddleware() {
-    const client = axios.create({ baseURL: env.get("KEPI_MIDDLEWARE_URL") });
+    const client = axios.create({ baseURL: env.get('KEPI_MIDDLEWARE_URL') })
 
-    await client.post("/register", {
-      url: env.get("API_URL"),
-      appKey: env.get("API_CODE"),
-    });
+    await client.post('/register', {
+      url: env.get('API_URL'),
+      appKey: env.get('API_CODE'),
+    })
   }
 }
