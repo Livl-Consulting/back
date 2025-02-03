dc8393528eb8621c2a1d30d63ce14b66a02fc3dc
Raffael Di Pietro <maxou4170@gmail.com>
2025-02-03 22:45:05 +0100

    feat(orders): mark as delivered

--- app/controllers/orders_controller.ts
+++ app/controllers/orders_controller.ts
@@ -1,114 +1,125 @@
 import Order from '#models/order'
-import { createOrderValidator, findOrderParamsValidator, updateOrderValidator } from '#validators/order'
+import {
+  createOrderValidator,
+  findOrderParamsValidator,
+  updateOrderValidator,
+} from '#validators/order'
 import type { HttpContext } from '@adonisjs/core/http'
 import Product from '../models/product.js'
-import Puppeteer from 'puppeteer';
+import Puppeteer from 'puppeteer'
 
 export default class OrdersController {
-    public async index({}: HttpContext) {
-        const orders = await Order.query().preload('client').preload('product').preload('clientPayments')
- 
-        return orders.map((order) => order.serialize())
+  public async index({}: HttpContext) {
+    const orders = await Order.query()
+      .preload('client')
+      .preload('product')
+      .preload('clientPayments')
+
+    return orders.map((order) => order.serialize())
+  }
+
+  public async store({ request }: HttpContext) {
+    const payload = await request.validateUsing(createOrderValidator)
+
+    // check if Order with quoteId already exists(if quoteId is provided)
+    if (payload.quoteId) {
+      const existingOrder = await Order.findBy('quoteId', payload.quoteId)
+      if (existingOrder) {
+        throw new Error('Order already exists')
+      }
     }
 
-    public async store({ request }: HttpContext) {
-        const payload = await request.validateUsing(createOrderValidator)
-
-        // check if Order with quoteId already exists(if quoteId is provided)
-        if (payload.quoteId) {
-            const existingOrder = await Order.findBy('quoteId', payload.quoteId)
-            if (existingOrder) {
-                throw new Error('Order already exists')
-            }
-        }
-
-        // Check if product type is 'sale'
-        const product = await Product.findOrFail(payload.productId)
-        if (product.type == 'purchase') {
-            throw new Error('You can only create opportunities for sale products')
-        }
-
-        const order = await Order.create(payload)
-        const refreshedOrder = await order.refresh()
-        return refreshedOrder.serialize()
+    // Check if product type is 'sale'
+    const product = await Product.findOrFail(payload.productId)
+    if (product.type == 'purchase') {
+      throw new Error('You can only create opportunities for sale products')
     }
 
-    public async show({ request }: HttpContext) {
-        const data = await request.validateUsing(findOrderParamsValidator)
+    const order = await Order.create(payload)
+    const refreshedOrder = await order.refresh()
+    return refreshedOrder.serialize()
+  }
 
-        const order =  await Order.query().where('id', data.params.id)
-            .preload('client')
-            .preload('product')
-            .preload('clientPayments')
-            .firstOrFail()
-        
-        return order.serialize()
+  public async show({ request }: HttpContext) {
+    const data = await request.validateUsing(findOrderParamsValidator)
+
+    const order = await Order.query()
+      .where('id', data.params.id)
+      .preload('client')
+      .preload('product')
+      .preload('clientPayments')
+      .firstOrFail()
+
+    return order.serialize()
+  }
+
+  public async update({ request }: HttpContext) {
+    const data = await request.validateUsing(findOrderParamsValidator)
+
+    const order = await Order.findOrFail(data.params.id)
+    const payload = await request.validateUsing(updateOrderValidator)
+    order.merge(payload)
+    await order.save()
+
+    return order.serialize()
+  }
+
+  public async generatePdf({ params, response, view }: HttpContext) {
+    try {
+      // Récupérer la commande avec le client et le produit associés
+      const order = await Order.query()
+        .where('id', params.id)
+        .preload('client')
+        .preload('product')
+        .firstOrFail()
+
+      // Générer le HTML en utilisant le template EdgeJS
+      const html = await view.render('order_confirmation', {
+        order,
+        client: order.client,
+        product: order.product,
+      })
+
+      // Lancer Puppeteer pour générer le PDF
+      const browser = await Puppeteer.launch({
+        executablePath: Puppeteer.executablePath(),
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-web-security',
+          '--disable-gpu',
+        ],
+      })
+
+      const page = await browser.newPage()
+      await page.setContent(html, { waitUntil: 'networkidle0' })
+
+      // Générer le PDF
+      const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true })
+
+      await browser.close()
+
+      const buffer = Buffer.isBuffer(pdfBuffer) ? pdfBuffer : Buffer.from(pdfBuffer)
+
+      // Envoyer le PDF en réponse
+      response.header('Content-Type', 'application/pdf')
+      response.header('Content-Disposition', `inline`) // to just display
+      // response.header('Content-Disposition', `attachment; filename="confirmation_commande_${order.id}.pdf"`);// to download
+      return response.send(buffer)
+    } catch (error) {
+      console.error('Erreur lors de la génération du PDF :', error)
+      return response.status(500).send('Une erreur est survenue lors de la génération du PDF.')
     }
+  }
 
-    public async update({ request }: HttpContext) {
-        const data = await request.validateUsing(findOrderParamsValidator)
+  public async destroy({ request }: HttpContext) {
+    const data = await request.validateUsing(findOrderParamsValidator)
 
-        const order = await Order.findOrFail(data.params.id)
-        const payload = await request.validateUsing(updateOrderValidator)
-        order.merge(payload)
-        await order.save()
+    const order = await Order.findOrFail(data.params.id)
 
-        return order.serialize()
-    }
+    order.status = 'cancelled'
+    await order.save()
 
-    public async generatePdf({ params, response, view }: HttpContext) {
-        try {
-            // Récupérer la commande avec le client et le produit associés
-            const order = await Order.query()
-                .where('id', params.id)
-                .preload('client')
-                .preload('product')
-                .firstOrFail();
-
-            // Générer le HTML en utilisant le template EdgeJS
-            const html = await view.render('order_confirmation', {
-                order,
-                client: order.client,
-                product: order.product,
-            });
-
-            // Lancer Puppeteer pour générer le PDF
-            const browser = await Puppeteer.launch({
-                executablePath:  Puppeteer.executablePath(),
-                args: [
-                  '--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security', '--disable-gpu',
-                ],
-              });
-                          
-            const page = await browser.newPage();
-            await page.setContent(html, { waitUntil: 'networkidle0' });
-
-            // Générer le PDF
-            const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
-
-            await browser.close();
-
-            const buffer = Buffer.isBuffer(pdfBuffer) ? pdfBuffer : Buffer.from(pdfBuffer);
-
-            // Envoyer le PDF en réponse
-            response.header('Content-Type', 'application/pdf');
-            response.header('Content-Disposition', `inline`); // to just display
-            // response.header('Content-Disposition', `attachment; filename="confirmation_commande_${order.id}.pdf"`);// to download
-            return response.send(buffer);
-        } catch (error) {
-            console.error('Erreur lors de la génération du PDF :', error);
-            return response.status(500).send('Une erreur est survenue lors de la génération du PDF.');
-        }
-    }
-
-    public async destroy({ request }: HttpContext) {
-        const data = await request.validateUsing(findOrderParamsValidator)
-
-        const order = await Order.findOrFail(data.params.id)
-
-        order.status = 'cancelled'
-        await order.save()
-
-        return { message: 'Order set to cancelled' }
-    }
+    return { message: 'Order set to cancelled' }
+  }
 }
--- app/validators/order.ts
+++ app/validators/order.ts
@@ -3,25 +3,25 @@ import { DateTime } from 'luxon'
 import { OrderStatus } from '../types/order_status.js'
 
 export const createOrderValidator = vine.compile(
-    vine.object({
-        clientId: vine.number(),
-        price: vine.number().min(0),
-        productId: vine.number(),
-        quoteId: vine.number().optional(),
-        dueDate: vine.date().transform((date) => DateTime.fromJSDate(date)),
-    })
+  vine.object({
+    clientId: vine.number(),
+    price: vine.number().min(0),
+    productId: vine.number(),
+    quoteId: vine.number().optional(),
+    dueDate: vine.date().transform((date) => DateTime.fromJSDate(date)),
+  })
 )
 
 export const findOrderParamsValidator = vine.compile(
-    vine.object({
-        params: vine.object({
-            id: vine.number()
-        })
-    })
+  vine.object({
+    params: vine.object({
+      id: vine.number(),
+    }),
+  })
 )
 
 export const updateOrderValidator = vine.compile(
-    vine.object({
-        status: vine.enum(['progress', 'delivered', 'invoiced', 'cancelled'] satisfies OrderStatus[]),
-    })
+  vine.object({
+    status: vine.enum(['progress', 'delivered', 'invoiced', 'cancelled'] satisfies OrderStatus[]),
+  })
 )
